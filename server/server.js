52c7f2eb55bc4be37de38bd726fccd751bc452d0
Asten Fuller <astenhfuller@gmail.com>
2020-01-04 16:19:08 -0800

    circleci

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(morgan('dev'));
-app.use(express.static('.circleci'));
 app.use(express.static('dist'));
 app.use(express.static('public'));
 
